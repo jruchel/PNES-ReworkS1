ba08dccf8ea4b1437b8bb69f9cf047af6755885a
jruchel <jruchel254@gmail.com>
2021-03-09 21:27:10 +0100

    Added a class for generating project wide unique ids for net elements

---
+++ src/main/java/org/ekipa/pnes/utils/IdGenerator.java
@@ -0,0 +1,26 @@
+package org.ekipa.pnes.utils;
+
+import org.ekipa.pnes.models.NetElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdGenerator {
+
+    private static List<NetElement> elementsList;
+
+    static {
+        elementsList = new ArrayList<>();
+    }
+
+    public static NetElement setId(NetElement element) {
+        if (elementsList.contains(element)) return element;
+        element.setId(String.format("%s-%d", element.getName(), findOccurrencesOfSameType(element)));
+        elementsList.add(element);
+        return element;
+    }
+
+    private static long findOccurrencesOfSameType(NetElement element) {
+        return elementsList.stream().filter(e -> e.getClass().equals(element.getClass())).count();
+    }
+}
