63ce523a62b579b8a2b669e8c6bcaba166d82cfb
jruchel <jruchel254@gmail.com>
2021-03-09 13:43:38 +0100

    Initial commit

---
+++ src/main/java/org/ekipa/pnes/PnesReworkApplication.java
@@ -0,0 +1,13 @@
+package org.ekipa.pnes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnesReworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PnesReworkApplication.class, args);
+    }
+
+}
