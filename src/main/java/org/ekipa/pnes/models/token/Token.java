9181f5dd2972a27cb82f2ed8d3f49048b65b2b75
jruchel <jruchel254@gmail.com>
2021-03-09 22:56:58 +0100

    Added pojo fields and methods to Token

--- src/main/java/org/ekipa/pnes/models/token/Token.java
+++ src/main/java/org/ekipa/pnes/models/token/Token.java
@@ -1,5 +1,14 @@
 package org.ekipa.pnes.models.token;
 
-public class Token {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.ekipa.pnes.models.NetElement;
 
+@AllArgsConstructor
+@Getter
+@Setter
+public class Token<V extends TokenValue> {
+    private NetElement parent;
+    private V value;
 }
