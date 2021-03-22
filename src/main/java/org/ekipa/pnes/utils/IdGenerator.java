98fc478656a7043c4081732179916a4639638cb3
MICZU-KOMPUTER\Miczu <miczu0607@gmail.com>
2021-03-22 17:24:04 +0100

    Created JavaDoc for IdGenerator Class

--- src/main/java/org/ekipa/pnes/utils/IdGenerator.java
+++ src/main/java/org/ekipa/pnes/utils/IdGenerator.java
@@ -13,10 +13,19 @@ public class IdGenerator {
         elementsList = new ArrayList<>();
     }
 
+    /**
+     * Czyści listę elementów
+     */
     public static void reset() {
         elementsList = new ArrayList<>();
     }
 
+    /**
+     * Sprawdza czy lista posiada podany element, jeśli tak zwraca ten element, jeśli nie ustawia podanemu elementowi id,
+     * następnie dodaje ten element do listy i zwraca go
+     * @param element pobiera element{@link org.ekipa.pnes.models.elements.NetElement}
+     * @return Zwraca podany element
+     */
     public static NetElement setId(NetElement element) {
         if (elementsList.contains(element)) return element;
         element.setId(String.format("%s%d", element.getClass().getSimpleName().charAt(0), findOccurrencesOfSameType(element)));
@@ -24,6 +33,11 @@ public class IdGenerator {
         return element;
     }
 
+    /**
+     * Zlicza ilość wystąpień typu podanego elementu następnie zwraca policzoną liczbę wystąpień
+     * @param element Pobiera element {@link org.ekipa.pnes.models.elements.NetElement}
+     * @return Zwraca liczbę wystąpień typu podanego elementu
+     */
     private static long findOccurrencesOfSameType(NetElement element) {
         return elementsList.stream().filter(e -> e.getClass().equals(element.getClass())).count();
     }
