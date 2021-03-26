b87b0b2cd40fd00324ccf377ba85bae804297209
MICZU-KOMPUTER\Miczu <miczu0607@gmail.com>
2021-03-27 00:34:27 +0100

    Implemented JavaFx to project created main class with simple window

--- src/main/java/org/ekipa/pnes/models/elements/NetElement.java
+++ src/main/java/org/ekipa/pnes/models/elements/NetElement.java
@@ -3,10 +3,7 @@ package org.ekipa.pnes.models.elements;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
