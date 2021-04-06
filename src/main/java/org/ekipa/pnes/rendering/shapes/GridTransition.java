8eefe1b256af8db7ac7c26ddda0e39efe73c88aa
Jakub Ruchel <jruchel254@gmail.com>
2021-04-06 17:55:41 +0200

    Changed access of GridArc constructors to private

--- src/main/java/org/ekipa/pnes/rendering/shapes/GridTransition.java
+++ src/main/java/org/ekipa/pnes/rendering/shapes/GridTransition.java
@@ -5,11 +5,11 @@ import javafx.scene.shape.Rectangle;
 import org.ekipa.pnes.models.elements.Transition;
 
 public class GridTransition extends GridNetElement {
-    public GridTransition(Transition netElement, Rectangle shape, Label label, OnGridElementAction onDelete, OnGridElementAction onCreate) {
+    private GridTransition(Transition netElement, Rectangle shape, Label label, OnGridElementAction onDelete, OnGridElementAction onCreate) {
         super(netElement, shape, label, onDelete, onCreate);
     }
 
-    public GridTransition(Transition netElement, Rectangle shape, OnGridElementAction onDelete, OnGridElementAction onCreate) {
+    private GridTransition(Transition netElement, Rectangle shape, OnGridElementAction onDelete, OnGridElementAction onCreate) {
         super(netElement, shape, onDelete, onCreate);
     }
 
